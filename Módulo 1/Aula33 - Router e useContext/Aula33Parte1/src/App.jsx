b95fbd8fe491ae97b07c0eaf95aca9efda5f1ba7
dionisio300 <dionisioassis80@gmail.com>
2025-05-20 10:53:58 -0300

    Aula 33 navegação

--- Módulo 1/Aula33 - Router e useContext/Aula33Parte1/src/App.jsx
+++ Módulo 1/Aula33 - Router e useContext/Aula33Parte1/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
+import {BrowserRouter} from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
+import AppRoutes from './routes/AppRoutes'
 
 function App() {
-
   return (
     <>
+      <BrowserRouter>
+          <Navbar/>
+          <AppRoutes/>
+      </BrowserRouter>
       <h1>React Router</h1>
     </>
   )
---
+++ Módulo 1/Aula33 - Router e useContext/Aula33Parte1/src/pages/Contato.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contato = () => {
+  return (
+    <div>
+        Essa é a página de contato
+    </div>
+  )
+}
+
+export default Contato
