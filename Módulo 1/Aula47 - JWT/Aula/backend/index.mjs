cf214922706a39f3951b6fd53795e488e9b854cc
dionisio300 <dionisioassis80@gmail.com>
2025-06-09 10:04:39 -0300

    Front inicial

---
+++ Módulo 1/Aula47 - JWT/Aula/backend/index.mjs
@@ -0,0 +1,50 @@
+/*
+criar o arquivo index.mjs
+criar o arquivo .env
+criar o arquivo .gitignore
+npm install express
+npm install mysql2
+npm install --save-dev nodemon
+npm install dotenv
+npm install bcrypt
+npm install cors
+npm init -y
+editar o package.json:
+"start":"node index.mjs",
+"dev": "nodemon index.mjs"
+*/
+import express from 'express'
+import cors from 'cors'
+import mysql from 'mysql2'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const porta = 3000
+
+// Conexão com o MySql
+const conexao = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'1234',
+    database:'escola1'
+})
+
+// Rotas com banco de dados
+conexao.connect((erro) => {
+    if(erro){
+        console.log(`Erro ao conectar com o banco: ${erro}`)
+    }else{
+        console.log('Banco conectado com sucesso!')
+    }
+})
+
+app.get('/',(req,res) => {
+    res.send('Servidor rodando')
+})
+
+
+app.listen(porta, ()=>{
+    console.log(`O servidor está rodando na porta ${porta}`)
+})
