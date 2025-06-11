f5b9140c2f9ee4dce1337e7c70fa329ca6714fe7
dionisio300 <dionisioassis80@gmail.com>
2025-06-11 09:22:43 -0300

    aula

---
+++ Módulo 1/Aula49 - Proteção de rotas/Aula/frontend/src/contexts/UserContext.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { createContext, useState } from 'react'
+
+export const UserContext = createContext(null)
+
+export const UserProvider = ({children}) => {
+
+    const [user, setUser] = useState()
+    return (
+    <UserContext.Provider value={{user, setUser}}>
+        {children}
+    </UserContext.Provider>
+)}
+
+
+
